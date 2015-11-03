fc6b0bf78d0bcc70c037952a9039e42d085964c9
Nikin Nagewadia <nikin.n@gmail.com>
2015-11-02 21:31:03 -0500

    fix issue due to scoping

--- script/production/script.js
+++ script/production/script.js
@@ -1,38 +1,84 @@
 $(function() {
 
+	var $input = $('.input');
+	var $start = $('.start');
+	var $stop = $('.stop');
+	var $digits = $('.digits');
+	var $reset = $('.reset');
+	// Extra Insurance to Ensure A Number is Passed
+	
+	var time;
+	var numInput;
+
+	// Countdown Timer
+	var countdown;
+
+	var timerStarted = false;
+
+
+
 	// Contols - start
 	function initialInput() {
-		$('.start').show();
-		$('.stop').hide();
-		$('.digits').show();
-		$('.reset').show();
+		$start.show();
+		$stop.hide();
+		$digits.show();
+		$reset.show();
 	}
 
 	function startTimer() {
-		$('.start').hide();
-		$('.stop').show();
-		$('.digits').hide();
-		$('.reset').hide();
+		$start.hide();
+		$stop.show();
+		$digits.hide();
+		$reset.hide();
+
+
+		console.log('timestart',time);
+	
+		
+
+		countdown = setInterval(function() {
+			// Formating of the time
+			var hours = Math.floor( time / 3600 ) % 24;
+			var minutes = Math.floor(time / 60) % 60;
+			var seconds = time % 60;
+			var timeFormat = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);		
+			$input.empty();
+			$('.output').show();
+			console.log(timeFormat);
+
+			// Update the span text with the number of seconds
+			time--;
+
+			$input.text( time );  // Prints the Time in Seconds on the DOM.
+
+			$('.output').text(timeFormat);  // Prints the Time in Hours Minutes & Seconds via the Console.
+			if (time < 0) {
+				clearInterval(countdown);
+				setTimeout(function() {
+					afterCompletion();
+				},5000);
+			}
+		},1000);
 	}
 
 	function pauseState() {
-		$('.start').show();
-		$('.stop').hide();
-		$('.digits').hide();
-		$('.reset').show();
+		$start.show();
+		$stop.hide();
+		$digits.hide();
+		$reset.show();
 	}
 
 	function restartState() {
-		$('.start').hide();
-		$('.stop').hide();
-		$('.digits').show();
-		$('.reset').hide();
+		$start.hide();
+		$stop.hide();
+		$digits.show();
+		$reset.hide();
 	}
 
 	function afterCompletion() {
-		$('.input').text('');
-		$('.digits').show();
-		$('.stop').hide();
+		$input.text('');
+		$digits.show();
+		$stop.hide();
 		$('.output').hide();
 	}
 	// Controls - end
@@ -41,60 +87,42 @@ $(function() {
 	// Each Digit represents a minute
 	$('.digit').on('click', function() {
 		var inputValue = $(this).data('seconds');
-		$('.input').show();
-		$('.input').append(inputValue);
+		$input.show();
+		$input.append(inputValue);
 		initialInput();
 	});
 
 
 	// Click start the timer begins
-	$('.start').on('click', function(){
+	$start.on('click', function(){
+		
+		$input.hide(); // Hide the input once the countdown starts
+		
+		if (timerStarted === false) {
+			numInput = $input.text();
+			time = parseInt(numInput);
+			console.log(time);
+			time = time * 60;
+			timerStarted = true;
+		}
+
 		startTimer();
 
-		$('.input').hide(); // Hide the input once the countdown starts
-		
+	});
 
-		// Extra Insurance to Ensure A Number is Passed
-		var time = parseInt($('.input').text() * 60);
-
-	
-		// Countdown Timer
-		var countdown = setInterval(function() {
-			$('.input').empty();
-			$('.output').show();
-
-
-			// Formating of the time
-			var hours = Math.floor( time / 3600 ) % 24;
-			var minutes = Math.floor(time / 60) % 60;
-			var seconds = time % 60;
-			var timeFormat = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
-
-
-			// Update the span text with the number of seconds
-			time--;
-			$('.input').text( time );  // Prints the Time in Seconds on the DOM.
-			$('.output').text(timeFormat);  // Prints the Time in Hours Minutes & Seconds via the Console.
-			if (time < 0) {
-				clearInterval(countdown);
-				setTimeout(function() {
-					afterCompletion();
-				},5000);
-			} else {
-				// if the user selects the stop button stop the countdown
-				$('.stop').on('click', function() {
-					clearInterval(countdown);
-					return pauseState();
-				});
-			}
-		},1000);
+	// if the user selects the stop button stop the countdown
+	$stop.on('click', function() {
+		clearInterval(countdown);
+		console.log('timeend',time);
+		return pauseState();
 	});
 
 	// Reset Button
-	$('.reset').on('click', function() {
-		$('.input').empty();
+	$reset.on('click', function() {
+		$input.empty();
 		$('.output').empty();
 		restartState();
-		$('.input').show();
+		$input.show();
+		timerStarted = false;
 	});
 });
