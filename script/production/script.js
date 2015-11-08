87c1a84ec4846b9593f577d389edffb6732a65fb
Nikin Nagewadia <nikin.n@gmail.com>
2015-11-08 17:00:11 -0500

    add jquery ui and style changes

--- script/production/script.js
+++ script/production/script.js
@@ -1,5 +1,4 @@
 $(function() {
-
     var $input = $('.input');
     var $start = $('.start');
     var $stop = $('.stop');
@@ -13,16 +12,16 @@ $(function() {
 
     // Contols - start
     function initialInput() {
-        $start.css('display', 'inline-block');
-        $stop.hide();
-        $digits.show();
-        $reset.css('display', 'inline-block');
+        $stop.fadeOut();
+        $digits.fadeIn();
+        $start.css('display', 'inline-block').fadeIn();
+        $reset.css('display', 'inline-block').fadeIn();
     }
 
     function startTimer() {
-        $start.hide();
-        $digits.hide();
-        $reset.hide();
+        $start.fadeOut();
+        $digits.fadeOut();
+        $reset.fadeOut();
         countdown = setInterval(function() {
             // Formating of the time
             var hours = Math.floor(time / 3600) % 24;
@@ -30,8 +29,8 @@ $(function() {
             var seconds = time % 60;
             var timeFormat = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
             $input.empty();
-            $stop.css('display', 'inline-block');
-            $output.show();
+            $stop.css('display', 'inline-block').fadeIn();
+            $output.fadeIn();
 
             // Update the span text with the number of seconds
             time--;
@@ -49,67 +48,147 @@ $(function() {
     }
 
     function pauseState() {
-        $start.css('display', 'inline-block');
-        $stop.hide();
-        $digits.hide();
-        $reset.css('display', 'inline-block');
+        $stop.fadeOut();
+        $digits.fadeOut();
+        setTimeout(function() {
+	        $start.fadeIn(function() {
+	        	$(this).css('display', 'inline-block').fadeIn();
+	        });
+	        $reset.fadeIn(function() {
+	        	$(this).css('display', 'inline-block').fadeIn();
+	        });
+        }, 500);
     }
 
     function restartState() {
-        $start.hide();
-        $stop.hide();
-        $digits.show();
-        $reset.hide();
+        $start.fadeOut();
+        $stop.fadeOut();
+        $digits.fadeIn();
+        $reset.fadeOut();
     }
 
     function afterCompletion() {
         $input.empty();
-        $digits.show();
-        $stop.hide();
-        $output.hide();
+        $input.fadeIn();
+        $digits.fadeIn();
+        $stop.fadeOut();
+        $output.fadeOut();
     }
     // Controls - end
 
 
     // Each Digit represents a minute
-    $('.digit').on('click', function() {
-        var inputValue = $(this).data('seconds');
-        $input.show();
-        $input.append(inputValue);
-        initialInput();
+    // $('.digit').on('click', function() {
+    //     var inputValue = $(this).data('seconds');
+    //     $input.show();
+    //     $input.append(inputValue);
+    //     initialInput();
+    // });
+    $('.digit').draggable({
+        cursor: 'crosshair',
+        opacity: 0.85,
+        revert: true,
+        start: function() {
+            $('body').css('background', 'gold');
+        },
+        stop: function() {
+            var inputValue = $(this).data('seconds');
+            $('body').css('background', 'snow');
+            $('.digit').css({
+            	'background' : 'white',
+            	'border' : '1px solid black',
+            	'color' : 'black'
+            });
+            $input.fadeIn();
+            $input.append(inputValue);
+            initialInput();
+        }
     });
-
 
     // Click start the timer begins
-    $start.on('click', function() {
+    // $start.on('click', function() {
 
-        $input.hide(); // Hide the input once the countdown starts
+    //     $input.hide(); // Hide the input once the countdown starts
 
-        // If the timer hasn't started multiply time by 60
-        if (timerStarted === false) {
-            numInput = $input.text();
-            time = parseInt(numInput);
-            time = time * 60;
-            timerStarted = true;
+    //     // If the timer hasn't started multiply time by 60
+    //     if (timerStarted === false) {
+    //         numInput = $input.text();
+    //         time = parseInt(numInput);
+    //         time = time * 60;
+    //         timerStarted = true;
+    //     }
+
+    //     startTimer();
+
+    // });
+    $start.draggable({
+        cursor: 'crosshair',
+        opacity: 0.85,
+        revert: true,
+        start: function() {
+            $('body').css('background', 'yellowgreen');
+        },
+        stop: function() {
+            $('body').css('background', 'snow');
+            $input.fadeOut(); // Hide the input once the countdown starts
+
+            // If the timer hasn't started multiply time by 60
+            if (timerStarted === false) {
+                numInput = $input.text();
+                time = parseInt(numInput);
+                time = time * 60;
+                timerStarted = true;
+            }
+
+            startTimer();
         }
-
-        startTimer();
-
     });
+
 
     // if the user selects the stop button stop the countdown
-    $stop.on('click', function() {
-        clearInterval(countdown);
-        return pauseState();
+    // $stop.on('click', function() {
+    //     clearInterval(countdown);
+    //     return pauseState();
+    // });
+    $stop.draggable({
+        cursor: 'crosshair',
+        opacity: 0.85,
+        revert: true,
+        start: function() {
+            $('body').css('background', 'darkred');
+        },
+        stop: function() {
+            $('body').css('background', 'snow');
+            clearInterval(countdown);
+            return pauseState();
+        }
     });
 
+
     // Reset Button
-    $reset.on('click', function() {
-        $input.empty();
-        $output.empty();
-        restartState();
-        $input.show();
-        $output.hide();
-        timerStarted = false;
+    // $reset.on('click', function() {
+    //     $input.empty();
+    //     $output.empty();
+    //     restartState();
+    //     $input.show();
+    //     $output.hide();
+    //     timerStarted = false;
+    // });
+    $reset.draggable({
+        cursor: 'crosshair',
+        opacity: 0.85,
+        revert: true,
+        start: function() {
+            $('body').css('background', 'orangered');
+        },
+        stop: function() {
+            $('body').css('background', 'snow');
+            $input.empty();
+            $output.empty();
+            restartState();
+            $output.hide();
+            $input.fadeIn();
+            timerStarted = false;
+        }
     });
 });
